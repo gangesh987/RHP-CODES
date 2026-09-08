ab740dfa55a4508c585e5ba02c65dc0a04fe4368
gangesh987 <gangeshrajaraman6028@gmail.com>
2026-09-08 10:08:16 +0530

    Update minus_two.java

--- minus_two.java
+++ minus_two.java
@@ -1,18 +1,27 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int t = sc.nextInt();
+
         while (t-- > 0) {
             int n = sc.nextInt();
-            int odd = 0, zero = 0, two = 0;
+            int odd = 0, div4 = 0, rem2 = 0;
+
             while (n-- > 0) {
                 int x = sc.nextInt();
-                if (x % 2 != 0) odd++;
-                else if (x % 4 == 0) zero++;
-                else two++;
+
+                if (x % 2 != 0)
+                    odd++;
+                else if (x % 4 == 0)
+                    div4++;
+                else
+                    rem2++;
             }
-            System.out.println(Math.max(odd, Math.max(zero, two)));
+
+            System.out.println(Math.max(odd, Math.max(div4, rem2)));
         }
     }
 }
