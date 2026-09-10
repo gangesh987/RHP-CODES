5d930317d20b9c9a6d7dc7ca273d436b7a051015
gangesh987 <gangeshrajaraman6028@gmail.com>
2026-09-10 09:15:29 +0530

    Add files via upload

---
+++ a_monocrap_contest.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        int zeros = 0;
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+
+            if (a[i] == 0)
+                zeros++;
+        }
+
+        if (zeros < 2) {
+            System.out.println(-1);
+        } else {
+            System.out.println(a[0] + a[n - 1]);
+        }
+    }
+}
